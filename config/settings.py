cf5683ed4aece2e25a93bbe96c363f8274f1a9d9
suhyun <id_suhyun@naver.com>
2024-08-13 18:21:07 +0900

    ✨feat: 6일차 챌린지-static 추가

--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_DIRS = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    STATIC_DIRS,
+]
+STATIC_ROOT = BASE_DIR / ".static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
